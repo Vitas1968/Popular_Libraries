fa9c61311f664d88186c85d434e3e9a2e0e69047
Vitas1968 <dor1968@mail.ru>
2020-03-15 14:42:26 +0700

    ссылки на R вынес в отдельный класс, который мы тут и реализуем через интерфейс

--- app/src/main/java/com/google/vitaly/mvpapp/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/com/google/vitaly/mvpapp/mvp/presenter/MainPresenter.kt
@@ -1,4 +1,28 @@
 package com.google.vitaly.mvpapp.mvp.presenter
 
-class MainPresenter {
+import com.google.vitaly.mvpapp.mvp.model.CountersModel
+import com.google.vitaly.mvpapp.mvp.view.MainView
+
+class MainPresenter(var view: MainView?, private val model: CountersModel, private val button: IGetBtnFromResources): IMainPresenter {
+
+    fun counterClick(id: Int){
+        var index=button.getBtnById(id)
+        if(index >= 0) {
+            when (index) {
+                0 -> updateBtnText(0)
+                1 -> updateBtnText(1)
+                2 -> updateBtnText(2)
+            }
+        }
+    }
+
+    private fun updateBtnText(index:Int) {
+        val nextValue = model.next(index)
+        view?.let {
+            it.setButtonText(index, nextValue.toString()) }
+    }
+
+    override fun detachView() {
+        view=null
+    }
 }
