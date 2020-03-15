abec72a58e95909dfa362ec190ca17ff4fada900
Vitas1968 <dor1968@mail.ru>
2020-03-15 14:39:35 +0700

    получаем из активити через презентер айдишник, проверяем какая кнопка нажата и возвращаем индекс

--- app/src/main/java/com/google/vitaly/mvpapp/mvp/model/util/GetBtnFromResourceImpl.kt
+++ app/src/main/java/com/google/vitaly/mvpapp/mvp/model/util/GetBtnFromResourceImpl.kt
@@ -1,4 +1,16 @@
 package com.google.vitaly.mvpapp.mvp.model.util
 
-class GetBtnFromResourceImpl {
+import android.content.Context
+import com.google.vitaly.mvpapp.R
+import com.google.vitaly.mvpapp.mvp.presenter.IGetBtnFromResources
+
+class GetBtnFromResourceImpl: IGetBtnFromResources {
+    override fun getBtnById(id: Int): Int {
+        return when(id){
+            R.id.btn_counter1 -> 0
+            R.id.btn_counter2 -> 1
+            R.id.btn_counter3 -> 2
+            else -> -1
+        }
+    }
 }
